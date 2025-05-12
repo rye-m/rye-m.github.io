015c3c488c8559f7c2a8837d12cce130f9b2c463
Rye M <ryem@dhcp-vl2051-323.eduroam.cornell.edu>
2025-05-12 11:45:01 -0400

    minor changes

--- JPG2png.sh
+++ JPG2png.sh
@@ -16,20 +16,20 @@ if [ ! -d "$dir" ]; then
 fi
 
 # Find all JPG and JPEG files (case insensitive) and convert them
-find "$dir" -type f -iname "*.png" | while read -r file; do
+find "$dir" -type f -iname "*.jpg" | while read -r file; do
     # Create the output filename by replacing the extension with .png
     output="${file%.*}.jpg"
     
     # echo "Converting: $file to $output"
     # ffmpeg -i "$file" -quality 50 -y "$output" > /dev/null 2>&1
-    ffmpeg -i "$file" -q:v 2 -y "$output" > /dev/null 2>&1
+    ffmpeg -i "$file" -q:v 2 -y "$output" #> /dev/null 2>&1
     
     # Check if conversion was successful
     if [ $? -eq 0 ]; then
         echo "Conversion successful: $output"
         
         # Delete the original JPG file
-        rm "$file"
+        # rm "$file"
         # echo "Deleted original file: $file"
     else
         echo "Error converting: $file - original file kept"
